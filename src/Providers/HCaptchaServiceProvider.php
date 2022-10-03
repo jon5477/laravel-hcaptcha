1d7e6c5dec796d2ce1f0ed8db5bba98d5a944d35
Jon Huang <huangjonathan@live.com>
2022-10-02 23:38:08 -0600

    Do not defer.

--- src/Providers/HCaptchaServiceProvider.php
+++ src/Providers/HCaptchaServiceProvider.php
@@ -2,14 +2,13 @@
 
 namespace jon5477\hCaptcha\Providers;
 
-use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use jon5477\hCaptcha\Builders\HCaptchaBuilder;
 
-class HCaptchaServiceProvider extends ServiceProvider implements DeferrableProvider
+class HCaptchaServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
@@ -57,15 +56,5 @@ class HCaptchaServiceProvider extends ServiceProvider implements DeferrableProvi
         $this->app->singleton(HCaptchaBuilder::class, function () {
             return new HCaptchaBuilder(Config::get('hcaptcha.api_site_key'), Config::get('hcaptcha.api_secret_key'));
         });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides(): array
-    {
-        return [HCaptchaBuilder::class];
     }
 }
